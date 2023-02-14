9c2764c64e4adc5739e3e41607dd29fdee5a2df7
Andres Castañeda <nosalesiano@gmail.com>
2023-02-14 00:16:28 -0500

    Cambios para solucion de vista

--- fly/urls.py
+++ fly/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 
-from .views import app
+from .views import app, index
 
 from estudiantesCursos.views import cursos, estudiantes, eliminarEstudiante, eliminarCurso
 
 
 urlpatterns = [
     
-    path('', lambda r: app(r, 'index.html')),
-    path('<path:resource>', app),
+    path("", index, name="app"),
+    path("app/", lambda r: app(r, 'index.html')),
+    path("app/<path:resource>", app),
 
     path("estudiantes/", estudiantes, name="estudiantes" ),
     path("estudiantes/<int:id>/", eliminarEstudiante, name="eliminarEstudiante" ),
--- fly/views.py
+++ fly/views.py
@@ -1,8 +1,13 @@
 from django.views.static import serve
 import os
+from django.shortcuts import redirect
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FLUTTER_WEB_APP = os.path.join(BASE_DIR, 'app')
 
+def index(request):
+    return redirect('/app/')
+    
 def app(request, resource):
     return serve(request, resource, FLUTTER_WEB_APP)
